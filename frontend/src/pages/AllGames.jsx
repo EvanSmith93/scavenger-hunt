db8a8cd78b8768e9263c72c0ab1f829897e363bf
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-07-13 15:49:44 -0600

    Remove bootstrap

--- frontend/src/pages/AllGames.jsx
+++ frontend/src/pages/AllGames.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ServerFacade from "../serverFacade/ServerFacade";
 import { Button, Card, Form, Input, Layout, List, Typography } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
